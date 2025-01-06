29be2ea67e70cd0204cb78c03112362c4aa39745
shok1122 <shok1122@zohomail.com>
2025-01-06 14:28:34 +0000

    Read data dir from env

--- harness/app/helper/option.go
+++ harness/app/helper/option.go
@@ -1,6 +1,8 @@
 package helper
 
 import (
+	"os"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/pborman/getopt/v2"
 )
@@ -25,13 +27,18 @@ var (
 )
 
 func SetupOpt() {
+	data_dir := os.Getenv("PRISM_HARNESS_DATA_DIR")
+	if data_dir == "" {
+		data_dir = "/var/lib/prism-harness"
+	}
+
 	HelpFlag = getopt.BoolLong("help", 'h', "display help")
 	SimFlag  = getopt.BoolLong("sim", 0, "simulation mode (disable blockchain)")
 	OptServer = getopt.StringLong("server", 0, "", "server's URL")
 	OptContractAddr = getopt.StringLong("contract", 0, "", "contract address")
 	OptSenderAddr = getopt.StringLong("sender-addr", 0, "", "sender's address")
 	OptSenderPrivKey = getopt.StringLong("sender-key", 0, "", "sender's private key")
-	OptPathCacheDir = getopt.StringLong("cache", 0, "/var/lib/prism/cache", "cache dir path")
-	OptPathLogFile = getopt.StringLong("log", 0, "/var/lib/prism/cache/prism.log", "log file path")
+	OptPathCacheDir = getopt.StringLong("cache", 0, data_dir + "/cache", "cache dir path")
+	OptPathLogFile = getopt.StringLong("log", 0, data_dir + "/cache/prism.log", "log file path")
 	OptDetectedList = getopt.StringLong("detected-list", 0, "", "detected list")
 }
