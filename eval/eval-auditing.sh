864f8dd1740583ffcc861f34e80e47b6f2d97b06
shok1122 <shok1122@zohomail.com>
2025-01-08 02:16:44 +0000

    Delete unused codes

--- eval/eval-auditing.sh
+++ eval/eval-auditing.sh
@@ -49,25 +49,3 @@ for block_num in `seq 100 100 1000`; do
 
     mv $PATH_LOG /opt/prism/logs/auditing-${block_num}.log
 done
-
-
-
-# auditing
-for ratio in `seq 0.1 0.1 1.0`; do
-    rm -rf $PATH_CACHE/*
-
-    $HARNESS setup $ADDRESS_0 $PRIVKEY_0 $ADDRESS_1 $PRIVKEY_1 
-    $HARNESS enroll auditor tpa1 $ADDRESS_2
-    $HARNESS enroll user    su1  $ADDRESS_4 $PRIVKEY_4
-
-    $AIDE testdata $PATH_TESTDATA 1000M 255
-    $HARNESS upload su1 $PATH_TESTDATA 1000
-
-    for i in `seq $TRIAL_COUNT`; do
-        $HARNESS challenge su1 $ratio 1.0
-        $HARNESS proof su1
-        $HARNESS audit tpa1 su1
-    done
-
-    mv $PATH_LOG /opt/prism/logs/auditing-${ratio}.log
-done
